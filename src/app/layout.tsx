9229988eaf7121f26d2ff7268dda1a6311dd5e9a
박세은 <seeunbag30@gmail.com>
2025-03-20 19:32:41 +0900

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: '박세은 프론트엔드 포트폴리오',
+  icons: {
+    icon: '/favicon.png',
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='ko'>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-red-500'>Home</h1>
+    </div>
+  )
+}
