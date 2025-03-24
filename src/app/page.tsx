d2176b0b3f01f1a6c7dc20d865f35e5fd3f10cda
박세은 <seeunbag30@gmail.com>
2025-03-24 18:18:13 +0900

    feat: navBar 버튼을 누른 페이지로 스크롤 이동하는 기능 구현현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,46 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
 import HomePage from '@/pages/homePage'
-import PortfolioPage from '@/pages/portfolioPage'
+import ProjectsPage from '@/pages/projectsPage'
 import SkillsPage from '@/pages/skillsPage'
 import ExperiencePage from '@/pages/experiencePage'
 import ContactPage from '@/pages/contactPage'
+import { pageSectionIndexMap } from '@/constants/pageSectionIndexMap'
+import useIndexedRefs from '@/hooks/useIndexedRefs'
 
 export default function Page() {
+  const params = useSearchParams()
+  const section = params?.get('section')
+
+  const { indexedRefs, setIndexedRef } = useIndexedRefs(pageSectionIndexMap.length)
+
+  useEffect(() => {
+    if (!section) return
+
+    const sectionIndex = pageSectionIndexMap[section]
+    indexedRefs.current[sectionIndex]?.scrollIntoView({ behavior: 'smooth' })
+  }, [section, indexedRefs])
+
   return (
-    <div>
-      <HomePage />
-      <PortfolioPage />
-      <SkillsPage />
-      <ExperiencePage />
-      <ContactPage />
-    </div>
+    <>
+      <div ref={setIndexedRef(0)}>
+        <HomePage />
+      </div>
+      <div ref={setIndexedRef(1)}>
+        <ProjectsPage />
+      </div>
+      <div ref={setIndexedRef(2)}>
+        <SkillsPage />
+      </div>
+      <div ref={setIndexedRef(3)}>
+        <ExperiencePage />
+      </div>
+      <div ref={setIndexedRef(4)}>
+        <ContactPage />
+      </div>
+    </>
   )
 }
