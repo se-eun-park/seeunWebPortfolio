a2d5b859d1433c761797f7e98c95c344c88775a3
박세은 <seeunbag30@gmail.com>
2025-03-26 23:16:37 +0900

    style: navBar css 작업업

--- src/components/NavBar/navBar.tsx
+++ src/components/NavBar/navBar.tsx
@@ -1,14 +1,29 @@
 import NavBarItem from './navBarItem'
+import { HomeIcon, FolderIcon, BagIcon, ToolIcon, AtIcon } from '@/assets/svg'
 
 export default function NavBar() {
   return (
-    <nav className='text-point-green w-full flex justify-center items-center fixed top-0'>
-      <div className='flex gap-7 w-fit bg-black'>
-        <NavBarItem path='home'>Home</NavBarItem>
-        <NavBarItem path='projects'>Projects</NavBarItem>
-        <NavBarItem path='skills'>Skills</NavBarItem>
-        <NavBarItem path='experience'>Experience</NavBarItem>
-        <NavBarItem path='contact'>Contact</NavBarItem>
+    <nav className='w-full flex justify-center items-center fixed top-5'>
+      <div className='flex items-center gap-10 px-8 py-3 w-fit bg-black-400 rounded-2xl'>
+        <NavBarItem path='home'>
+          <HomeIcon width={28} height={28} fill='#FFFFFF' />
+        </NavBarItem>
+
+        <NavBarItem path='projects'>
+          <FolderIcon width={28} height={28} fill='#FFFFFF' />
+        </NavBarItem>
+
+        <NavBarItem path='experience'>
+          <BagIcon width={28} height={28} fill='#FFFFFF' />
+        </NavBarItem>
+
+        <NavBarItem path='skills'>
+          <ToolIcon width={28} height={28} />
+        </NavBarItem>
+
+        <NavBarItem path='contact'>
+          <AtIcon width={28} height={28} fill='#FFFFFF' />
+        </NavBarItem>
       </div>
     </nav>
   )
--- src/components/NavBar/navBarItem.tsx
+++ src/components/NavBar/navBarItem.tsx
@@ -1,18 +1,55 @@
 'use client'
 
+import { useState } from 'react'
+
 type NavBarItemProps = {
   children: React.ReactNode
   path: string
 }
 
+type HoverTextMapProps = {
+  [key: string]: string
+}
+
+const hoverTextMap: HoverTextMapProps = {
+  home: '홈',
+  projects: '프로젝트',
+  experience: '활동',
+  skills: '기술 스택',
+  contact: '연락처',
+}
+
 export default function NavBarItem({ children, path }: NavBarItemProps) {
+  const [isHover, setIsHover] = useState(false)
+
   const handleOnClick = () => {
     window.history.pushState(null, '', `/?section=${path}`)
   }
 
+  const handleOnMouseEnter = () => {
+    setIsHover(true)
+  }
+
+  const handleOnMouseLeave = () => {
+    setIsHover(false)
+  }
+
   return (
-    <button onClick={handleOnClick} className='cursor-pointer'>
+    <button
+      onClick={handleOnClick}
+      onMouseEnter={handleOnMouseEnter}
+      onMouseLeave={handleOnMouseLeave}
+      className='cursor-pointer relative'
+    >
       {children}
+
+      <div
+        className={`absolute ${
+          isHover ? 'opacity-100 top-11' : 'opacity-0 top-4'
+        } transition-all duration-500 ease-in-out left-1/2 -translate-x-1/2 bg-black-300 px-3 py-0.5 rounded-lg text-sm pointer-events-none`}
+      >
+        <p className='text-white min-w-max text-center font-pretendard-400'>{hoverTextMap[path]}</p>
+      </div>
     </button>
   )
 }
