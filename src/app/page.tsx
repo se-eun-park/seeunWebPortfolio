c6e97dc10841e872510021290331fb82367f63d2
박세은 <seeunbag30@gmail.com>
2025-03-28 22:16:43 +0900

    truck: navBar위치 layout -> page로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Suspense } from 'react'
 import PageContents from '@/components/pageContents'
+import NavBar from '@/components/NavBar/navBar'
 
 export default function Page() {
   return (
     <Suspense fallback={null}>
+      <NavBar />
       <PageContents />
     </Suspense>
   )
